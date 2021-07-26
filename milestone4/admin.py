bbb6905c132e758a06186d0a09b3c8bb5398263e
saoirse-defi <saoirsef@yahoo.co.uk>
2021-07-26 12:05:45 +0000

    register models

--- milestone4/admin.py
+++ milestone4/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Buyer, Seller, Listing, Sale, Rating
 
 # Register your models here.
+admin.site.register(Buyer)
+admin.site.register(Seller)
+admin.site.register(Listing)
+admin.site.register(Sale)
+admin.site.register(Rating)
