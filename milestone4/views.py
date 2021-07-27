6dd69ef5f2164e7ae416e11395969e2132d303ea
saoirse-defi <saoirsef@yahoo.co.uk>
2021-07-27 14:05:15 +0000

    add register view

--- milestone4/views.py
+++ milestone4/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, redirect
+from .models import Seller
+from .forms import SellerForm
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = SellerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    form = SellerForm()
+    context = {'form': form}
+    return render(request, 'register/register.html', context)
+
