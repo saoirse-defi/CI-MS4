d618deceaf241b823730a1e0ab4b55638422e000
saoirse-defi <saoirsef@yahoo.co.uk>
2021-07-28 16:28:31 +0000

    add allauth package

--- CI_MS4_DJANGO/urls.py
+++ CI_MS4_DJANGO/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from milestone4.views import index, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
-    path('register_seller/', register, name='register')
+    path('register_seller/', register, name='register'),
+    path('accounts/', include('allauth.urls'))
 ]
